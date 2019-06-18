e272ab17ad7249493b76b2c0acd3afea7aec4fed
Sam Barrie <sam.dbarrie@gmail.com>
2019-06-18 12:33:36 +0100

    added edit and delete buttons and functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :find_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @pages = Page.all
   end
@@ -12,11 +14,27 @@ class PagesController < ApplicationController
     redirect_to page
   end
 
+  def edit
+  end
+
   def show
-    @page = Page.find params[:id]
+  end
+
+  def update
+    @page.update page_params
+    redirect_to page_path(@page)
+  end
+
+  def destroy
+    @page.destroy
+    redirect_to pages_path
   end
 
   private
+
+  def find_page
+    @page = Page.find params[:id]
+  end
 
   def page_params
     params.require(:page).permit(:title, :content, :imgurl)
