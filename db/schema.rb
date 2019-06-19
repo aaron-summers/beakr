ddde3e04431722268141adccdb20a828a7bc3145
Aaron Summers <aaron.summers2797@gmail.com>
2019-06-19 10:39:15 +0100

    login signup styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_160505) do
+ActiveRecord::Schema.define(version: 2019_06_18_155425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2019_06_18_160505) do
     t.string "imgurl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_pages_on_user_id"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
@@ -45,5 +43,4 @@ ActiveRecord::Schema.define(version: 2019_06_18_160505) do
 
   add_foreign_key "likes", "pages"
   add_foreign_key "likes", "users"
-  add_foreign_key "pages", "users"
 end
