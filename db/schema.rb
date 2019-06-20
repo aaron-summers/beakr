7101d9a5f549b6743d2463dcb9df352674fc8fe3
Sam Barrie <sam.dbarrie@gmail.com>
2019-06-20 11:28:44 +0100

    added styling to the datalist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_135814) do
+ActiveRecord::Schema.define(version: 2019_06_19_153219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_06_19_135814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "tag_id"
+    t.index ["tag_id"], name: "index_pages_on_tag_id"
     t.index ["user_id"], name: "index_pages_on_user_id"
   end
 
@@ -61,5 +63,6 @@ ActiveRecord::Schema.define(version: 2019_06_19_135814) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "pages"
   add_foreign_key "likes", "users"
+  add_foreign_key "pages", "tags"
   add_foreign_key "pages", "users"
 end
