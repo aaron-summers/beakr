0f276c882dd42bf0f0b948d5408f087508ab66ca
Sam Barrie <sam.dbarrie@gmail.com>
2019-06-21 11:18:11 +0100

    adding custom 404s

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,63 @@ module Bibliomania
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # => Custom 404s
+
+    # => This is an example of ALL available config options
+    # => You're able to see exactly how it works here:
+    # => https://github.com/richpeck/exception_handler/blob/master/lib/exception_handler/config.rb
+
+    # => Config hash (no initializer required)
+    config.exception_handler = {
+      dev:        nil, # allows you to turn ExceptionHandler "on" in development
+      db:         nil, # allocates a "table name" into which exceptions are saved (defaults to nil)
+      email:      nil, # sends exception emails to a listed email (string // "you@email.com")
+
+      # Custom Exceptions
+      custom_exceptions: {
+        #'ActionController::RoutingError' => :not_found # => example
+      },
+
+      # This is an entirely NEW structure for the "layouts" area
+      # You're able to define layouts, notifications etc ↴
+
+      # All keys interpolated as strings, so you can use symbols, strings or integers where necessary
+      exceptions: {
+
+        :all => {
+          layout: "exception", # define layout
+          notification: true, # (false by default)
+          deliver: #something here to control the type of response
+        },
+        :4xx => {
+          layout: nil, # define layout
+          notification: true, # (false by default)
+          deliver: #something here to control the type of response
+        },
+        :5xx => {
+          layout: "exception", # define layout
+          notification: true, # (false by default)
+          deliver: #something here to control the type of response
+        },
+        500 => {
+          layout: "exception", # define layout
+          notification: true, # (false by default)
+          deliver: #something here to control the type of response
+        },
+
+        # This is the old structure
+        # Still works but will be deprecated in future versions
+
+        501 => "exception",
+        502 => "exception",
+        503 => "exception",
+        504 => "exception",
+        505 => "exception",
+        507 => "exception",
+        510 => "exception"
+
+      }
+    }
   end
 end
